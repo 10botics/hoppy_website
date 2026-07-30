8bc24a89e12e3e69ef7e18cab38f6e3dbfc400d5
LTH527 <1benedictlee@gmail.com>
2026-07-30 15:39:08 +0800

    fix: improve social link previews

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.hoppy.day",
   i18n: {
     defaultLocale: "tc",
     locales: ["tc", "en"],
